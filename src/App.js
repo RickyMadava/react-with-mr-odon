ab22a3f3986a5e3df9e3ca782ecd0d5af7047703
RickyMadava <bertrandricky02@gmail.com>
2023-02-11 20:11:07 +0300

    Ajout du framework tailwind

--- src/App.js
+++ src/App.js
@@ -21,6 +21,46 @@ function App() {
           <MainMenuBtn text="About" />
         </div>
         <div className="components">
+          <div class="flex items-center z-30">
+            <svg
+              class="h-6 w-6 mr-2 text-yellow-300 dark:text-gray-200"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              />
+            </svg>
+            <div class="relative inline-block w-10 select-none">
+              <input
+                type="checkbox"
+                name="toggleTheme"
+                id="toggleTheme"
+                class="transition-transform transform translate-x-0 duration-500 theme-switch absolute block w-5 h-5 rounded-full bg-white appearance-none cursor-pointer mx-1 my-1"
+              />
+              <label
+                for="toggleTheme"
+                class="theme-switch-label block overflow-hidden w-14 h-7 rounded-full bg-yellow-300 cursor-pointer"
+              ></label>
+            </div>
+            <svg
+              class="h-6 w-6 ml-6 text-gray-900 dark:text-yellow-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+              />
+            </svg>
+          </div>
           <div className="components-btn-notification">
             <img src={bell} alt="Bell icon" />
             <div className="btn-notification">
